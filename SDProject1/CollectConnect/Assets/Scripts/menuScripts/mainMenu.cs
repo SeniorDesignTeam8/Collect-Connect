3db6ca2bec1e1b659f56fec5829f0667c67ae6e2
Alexandra <akalioto@umich.edu>
2016-12-04 01:55:03 -0500

    1. Added menu so can "start game" on load of scene. 2. Added placeholders for cards 3. Resized and formatted board to match Nicole's design update (I know scaling is off.) 4. To do: functionality for buttons, labels/text fields

---
+++ SDProject1/CollectConnect/Assets/Scripts/menuScripts/mainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+    public Texture backgroundTexture;
+    public Texture titleText;
+    public GUIStyle btn;
+
+    void OnGUI()
+    {
+        //display background texture
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+
+        GUI.DrawTexture(new Rect(Screen.width / 4, Screen.height - Screen.height, Screen.width * 0.5f, Screen.height * 0.5f), titleText);
+
+        //display buttons (without GUI Outline)
+        if (GUI.Button(new Rect(Screen.width * 0.35f, Screen.height * 0.65f, Screen.width * 0.3f, Screen.height * 0.2f), "", btn))
+        {
+            OnMouseDown();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
