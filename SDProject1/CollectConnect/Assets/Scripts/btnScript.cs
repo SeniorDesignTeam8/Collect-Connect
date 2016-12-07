b18fa25536a1d339aa6b3539de6f4e974af245d8
Alexandra <akalioto@umich.edu>
2016-12-07 14:03:31 -0500

    Redesigned gameboard and added buttons. Buttons are not currently in place; I am working on having them "generate" in the correct spot.

---
+++ SDProject1/CollectConnect/Assets/Scripts/btnScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btnScript : MonoBehaviour {
+
+    public void OnStartGame()
+    {
+       Debug.Log("Do button functionality");
+
+        //insert button functionality here
+    }
+
+}
