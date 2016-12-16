e7120f74690c53f5c7616852860b61386ba7f582
aaronjgarnett <aaronjgarnett@gmail.com>
2016-12-15 21:38:49 -0500

    ButtonManager

---
+++ SDProject1/CollectConnect/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour
+{
+    private Button _yourbutton;
+
+    // Use this for initialization
+    void Start()
+    {
+        _yourbutton = GetComponent<Button>();
+    }
+
+    void OnPointerClick()
+    {
+        Debug.Log(_yourbutton);
+    }
+}
