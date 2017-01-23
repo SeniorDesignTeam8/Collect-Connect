fb8dc84094b1330b6a4d0f9891cc59bc104692a0
Jake_kuznicki <Jakekuznicki@live.com>
2017-01-23 12:51:38 -0500

    here mike

---
+++ SDProject1/CollectConnect/Assets/Scripts/NodeMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeMovement : MonoBehaviour
+{
+
+
+    private Vector3 currentPosition;
+	// Use this for initialization
+	void Start ()
+	{
+	    currentPosition = transform.localPosition;
+	}
+
+    private void OnMouseDrag()
+    {
+        gameObject.transform.localPosition = Input.mousePosition;
+    }
+
+    private void OnMouseUp()
+    {
+        currentPosition = transform.localPosition;
+    }
+    
+}
