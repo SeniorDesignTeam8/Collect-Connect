21032788b10b2fabbb77524fc6e4077a544b9641
Alexandra <akalioto@umich.edu>
2017-02-03 17:56:00 -0500

    Vetting added. Vetting button isn't currenly working, so when you click on it, it wil not do anything.

---
+++ SDProject1/CollectConnect/Assets/VetBtnHit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VetBtnHit : MonoBehaviour {
+
+    public void on_btn_click()
+    {
+        BoardManager.Instance.VetBtnSelected();
+        Debug.Log("Got here 1");
+    }
+}
