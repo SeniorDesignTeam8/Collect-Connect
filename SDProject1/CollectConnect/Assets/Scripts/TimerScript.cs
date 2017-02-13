36949c0d8168bb5464eb521f01155b0207b8c8e3
aaronjgarnett <aaronjgarnett@gmail.com>
2017-02-13 12:45:57 -0500

    Timer: Turns Red when below 15 seconds.              Stops while holding the space bar.              Resets when clicked every three times.

--- SDProject1/CollectConnect/Assets/Scripts/TimerScript.cs
+++ SDProject1/CollectConnect/Assets/Scripts/TimerScript.cs
@@ -7,23 +7,41 @@ public class TimerScript : MonoBehaviour
 {
     public static int Timeleft;
     private BoardManager bM;
+    private Button rButton;
+    private int numOfClicks = 0;
     Text t;
 
     // Use this for initialization
     void Start()
     {
         t = GetComponent<Text>();
+        rButton = GetComponent<Button>();
         bM = FindObjectOfType<BoardManager>();
         InvokeRepeating("decreaseTime", 1, 1);
+        rButton.onClick.AddListener(resetTimer);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Timeleft < 15)
+        {
+            t.color = Color.red;
+        }
+        else
+        {
+            t.color = Color.black;
+        }
+
         if (Timeleft < 0)
         {
             bM.PassBtnHit();
         }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            CancelInvoke();
+        if(Input.GetKeyUp(KeyCode.Space))
+            InvokeRepeating("decreaseTime", 1, 1);
 
         t.text = "" + Timeleft;
     }
@@ -31,5 +49,12 @@ public class TimerScript : MonoBehaviour
     void decreaseTime()
     {
         Timeleft--;
+    }
+
+    void resetTimer()
+    {
+        numOfClicks++;
+        if (numOfClicks%3 == 0)
+            Timeleft = 90;
     }
 }
