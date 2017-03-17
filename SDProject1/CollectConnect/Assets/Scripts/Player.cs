62ea4d858fa3635b7d066855fa883ad075431d79
Jake_kuznicki <Jakekuznicki@live.com>
2017-03-17 17:03:02 -0400

    Changed AI passing thresholds

--- SDProject1/CollectConnect/Assets/Scripts/Player.cs
+++ SDProject1/CollectConnect/Assets/Scripts/Player.cs
@@ -68,7 +68,7 @@ public class Player : MonoBehaviour
 
     private static readonly float[] AiPassThresholds =
     {
-        0.05f, 0.25f, 0.2f, 0.25f
+        0.05f, 0.05f, 0.05f, 0.05f
     };
 
     private void Start()
