aefacd27ea4bc09d9a2c5a2dfaba25766dd2d807
aaronjgarnett <aaronjgarnett@gmail.com>
2017-04-03 19:53:48 -0400

    Timer might be fixed?

--- SDProject1/CollectConnect/Assets/Scripts/BoardManager.cs
+++ SDProject1/CollectConnect/Assets/Scripts/BoardManager.cs
@@ -512,9 +512,9 @@ public class BoardManager : MonoBehaviour
 
                     CurrentPlayer = 0;    //start round after voting (for late update)
                     CurrentPhase = GamePhase.Playing;
-                    _ts.InvokeRepeating("DecreaseTime", 1, 1);
                 }
             }
+            _ts.InvokeRepeating("DecreaseTime", 1, 1);
         }
     }
 
@@ -563,7 +563,6 @@ public class BoardManager : MonoBehaviour
 
         TimerScript.Timeleft = 90;
         _ts.CircleSlider.fillAmount = 1.0f;
-        _ts.InvokeRepeating("DecreaseTime", 1, 1);
 
         if (CurrentPhase != GamePhase.Voting)
         {
