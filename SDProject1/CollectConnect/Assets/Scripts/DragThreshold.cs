9f08d1d1fa3a28d6bc8d5d8d11191c3f29ea99cc
Aristotelis Papaioannou <adpapaio@gmail.com>
2017-08-02 15:46:23 -0400

    Unignored the script folder and some other minor files
    This is what was preventing a proper clone of the repository from our
    "main" machine

---
+++ SDProject1/CollectConnect/Assets/Scripts/DragThreshold.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragThreshold : MonoBehaviour {
+
+	void Start () {
+		int defaultValue = EventSystem.current.pixelDragThreshold;		
+		EventSystem.current.pixelDragThreshold = 
+			Mathf.Max(
+				defaultValue , 
+				(int) (defaultValue * Screen.dpi / 160f));
+
+		Debug.Log (EventSystem.current.pixelDragThreshold);
+	}
+	
+
+}
---
+++ SDProject1/CollectConnect/Assets/Scripts/GetImage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetImage : MonoBehaviour {
+
+	public SpriteRenderer NewImage;
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.GetComponent<SpriteRenderer> ().sprite = NewImage.sprite;
+	}
+
+}
