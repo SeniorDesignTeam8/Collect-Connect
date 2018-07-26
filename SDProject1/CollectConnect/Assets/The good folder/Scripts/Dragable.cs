6dc4c1063a89e6ad0da91bdaae4a4acc1c36958b
haddadsa <haddadsa@umich.edu>
2018-07-26 13:52:52 -0400

    card dragging, board set up

---
+++ SDProject1/CollectConnect/Assets/The good folder/Scripts/Dragable.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Dragable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    CanvasGroup cgroup;
+    public Transform lastLocation;
+    public Transform hand;
+    public  GameObject moveArea;
+    RectTransform rectTrans;
+    GameObject placeholder;
+
+
+    //when the card is selected it 
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(transform.parent);
+        RectTransform rt = placeholder.AddComponent<RectTransform>();
+        rt = rectTrans;
+        placeholder.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
+        lastLocation = transform.parent;
+        transform.SetParent(moveArea.transform);
+        cgroup.blocksRaycasts = false;
+    }
+
+    // events that can occur while moving the card around the screen 
+    //
+    public void OnDrag(PointerEventData eventData)
+    {      
+        transform.position = eventData.position;
+       checkDrop area= hand.GetComponent<checkDrop>();
+        if(area.ableArrange)
+            arrangeHand();
+
+      
+    }
+    
+    
+    // When the player lets go of the card it will either stay where they dropped it if valid
+    //or snap back to the last valid location 
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(lastLocation);
+        transform.SetSiblingIndex ( placeholder.transform.GetSiblingIndex());
+        Destroy(placeholder);
+        
+        cgroup.blocksRaycasts = true;
+    }
+
+    void arrangeHand()
+    {
+        for (int i = 0; i < lastLocation.childCount; i++)
+        {
+            if (transform.position.x < lastLocation.GetChild(i).position.x)
+            {
+                placeholder.transform.SetSiblingIndex(i);
+                break;
+            }
+        }
+    }
+
+
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        rectTrans = GetComponent<RectTransform>();
+        cgroup = GetComponent<CanvasGroup>();
+        moveArea = GameObject.Find("mainCanvas");
+       hand= lastLocation = transform.parent;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ SDProject1/CollectConnect/Assets/The good folder/Scripts/checkDrop.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class checkDrop : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public bool ableArrange;
+    bool isValidSpot;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+       
+            d.lastLocation = transform;
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (transform.tag == "hand")
+        {
+            ableArrange = true;
+        }
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(transform.tag=="hand")
+        {
+            ableArrange = false;
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        ableArrange = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
